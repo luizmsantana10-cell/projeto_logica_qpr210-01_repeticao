bfd96d1f678520ce7a5d73cfff3b19bcc2408f18
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-16 16:38:38 -0300

    js alteraçao

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -2,5 +2,10 @@
 const divFor = document.querySelector('#div-for')
 for(i = 0;i < 10; i++ ){
     console.log(i, 'Amor')
-
+    divFor.innerHTML += `${i} - Amor <br> `
 }
+
+// contador acum
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
