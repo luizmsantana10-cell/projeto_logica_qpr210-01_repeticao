a31a88a453abe6a91ed49f1e7e055bacbbc95b99
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-16 16:51:09 -0300

    js

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -9,3 +9,19 @@ for(i = 0;i < 10; i++ ){
 const inputNum = document.querySelector('#num')
 const btnNum = document.querySelector('#btn-num')
 const divContAcum = document.querySelector('#div-cont-acum')
+
+let cont = 0
+let acum = 0.0
+
+btnNum.addEventListener('click', (evt)=>{
+    let numDigitado = Number(inputNum.value)
+
+    cont++
+    acum += numDigitado 
+
+    divContAcum.innerHTML = `TOTAL DE NUMERO DIGITADO É: ${cont
+} <br> A SOMA DOS NUMEROS É: ${ACUM}`
+
+    inputNum.value = ''
+
+})
