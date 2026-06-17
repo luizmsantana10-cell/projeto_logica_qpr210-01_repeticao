3bffb3b64e29df6e37b58e9a1cd52bd02fa449de
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-17 13:48:24 -0300

    alterado scrpit e htlm

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -1,27 +1,55 @@
-//estrutura repetiçao for
+// estrutura repetição for
 const divFor = document.querySelector('#div-for')
-for(i = 0;i < 10; i++ ){
+
+for(let i = 0; i < 10; i++){
     console.log(i, 'Amor')
-    divFor.innerHTML += `${i} - Amor <br> `
+    divFor.innerHTML += `${i} - Amor <br>`
 }
 
-// contador acum
+
+// contador acumulador
 const inputNum = document.querySelector('#num')
 const btnNum = document.querySelector('#btn-num')
 const divContAcum = document.querySelector('#div-cont-acum')
 
 let cont = 0
-let acum = 0.0
+let acum = 0
 
-btnNum.addEventListener('click', (evt)=>{
+btnNum.addEventListener('click', ()=>{
+
     let numDigitado = Number(inputNum.value)
 
     cont++
-    acum += numDigitado 
+    acum += numDigitado
 
-    divContAcum.innerHTML = `TOTAL DE NUMERO DIGITADO É: ${cont
-} <br> A SOMA DOS NUMEROS É: ${ACUM}`
+    divContAcum.innerHTML = `
+    TOTAL DE NÚMEROS DIGITADOS: ${cont} <br>
+    A SOMA DOS NÚMEROS É: ${acum}
+    `
 
     inputNum.value = ''
+
+})
+
+
+// repetir frase usando for
+const inputFrase = document.querySelector('#frase')
+const inputNumRepeticao = document.querySelector('#num-repeticao')
+const btnFrase = document.querySelector('#btn-frase')
+const divFrase = document.querySelector('#div-frase')
+
+
+btnFrase.addEventListener('click', ()=>{
+
+    let frase = inputFrase.value
+    let quantidade = Number(inputNumRepeticao.value)
+
+    divFrase.innerHTML = ''
+
+    for(let i = 1; i <= quantidade; i++){
+
+        divFrase.innerHTML += `${i} - ${frase}<br>`
+
+    }
 
 })
