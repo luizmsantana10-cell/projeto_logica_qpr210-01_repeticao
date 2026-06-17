77be4a80f828c460ceebb77fc9f17dc54eacfa81
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-17 15:53:24 -0300

    script altercao

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -42,14 +42,39 @@ const divFrase = document.querySelector('#div-frase')
 btnFrase.addEventListener('click', ()=>{
 
     let frase = inputFrase.value
-    let quantidade = Number(inputNumRepeticao.value)
+    let numRepeti = Number(inputNumRepeticao.value)
 
     divFrase.innerHTML = ''
 
-    for(let i = 1; i <= quantidade; i++){
+    for(let i = 1; i <= numRepeti; i++){
 
         divFrase.innerHTML += `${i} - ${frase}<br>`
 
     }
 
 })
+
+//colecao de dados - array
+const presentes = ['Bicicleta', 'celular' ,'Camisa do Mengao', 'Doleira', 'Tenis adidas']
+
+const divArray =document.querySelector('div-array')
+
+//div array.inner
+
+for(i= 0;i < 5; i++){
+    divArray.innerHTML += `${presentes[i]} <br>`
+}
+
+//FOR IN
+const divForIn = document.querySelector('#div-forin')
+
+for(let posicao in presentes){
+    divForIn.innerHTML += `${presentes[posicao]} <br>`
+}
+
+//FOR OF
+const divForOf = document.querySelector('#div-forof')
+
+for (let elemento of pessoas){
+    divForOf.innerHTML += `${elemento} <br>`
+}
